7a5127644167c2323f0dfcec744eb108d2454c56
baesparza <bruno.be81@gmail.com>
2019-02-04 23:18:09 -0500

    style: fix project cards

--- components/cards/ProjectCard.vue
+++ components/cards/ProjectCard.vue
@@ -89,8 +89,8 @@ export default {
     transition: all 0.5s;
   }
   &-details {
-    max-height: 100%;
-    max-width: 100%;
+    height: 100%;
+    width: 100%;
     padding: 15px;
     overflow: hidden;
 
